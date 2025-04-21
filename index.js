1b381586708ba6fada8a0cf9720ce0d92a2e2d44
Jehovah-Nissi <jehovahnissi6384@gmail.com>
2025-04-21 17:22:46 +0100

    introduced interractivity

--- index.js
+++ index.js
@@ -23,3 +23,25 @@ menuButton.addEventListener('click', () => {
     sideBar.classList.toggle('collapsed');
     main.classList.toggle('collapsed');
 });
+
+fetch('videos.json')
+    .then(response => response.json())
+    .then(videos => {
+        videos.forEach(video => {
+            const videoElement = document.createElement('video');
+            videoElement.src = video.url;
+            videoElement.preload = "metadata";
+
+            videoElement.addEventListener('loadedmetadata', () => {
+                const duration = videoElement.duration;
+                const minutes = Math.floor(duration / 60);
+                const seconds = Math.floor(duration % 60);
+
+                const paddedSeconds = seconds < 10 ? `0${seconds}` : seconds;
+                const videoLenght = `${minutes}:${paddedSeconds}`;
+            });
+        });
+    })
+    .catch(error => {
+        console.log('Error loading video');
+    });
